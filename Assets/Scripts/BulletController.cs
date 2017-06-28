214b5b34b87fc92ca4a2c9f294993ba0cad1aa04
CabbageWarrior <feelthecabbage@gmail.com>
2017-06-28 22:42:37 +0200

    Added ground explosion when bullet collides. Added bullet logic.

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    /// <summary>
+    /// Global reference to the Rigidbody2D.
+    /// </summary>
+    private Rigidbody2D rb;
+
+    /// <summary>
+    /// Transform component of the Sprite into the Bullet object.
+    /// </summary>
+    public Transform bulletSpriteTransform;
+    
+    /// <summary>
+    /// Bool that says whether or not to update the rotation of the GameObject Sprite based on the trajectory of the bullet. This bool is to say that after the bullet collides with some other body, the rotation of the bullet should no longer be updated based on the trajectory.
+    /// </summary>
+    private bool updateAngle = true;
+
+    /// <summary>
+    /// Global reference to the BulletSmoke object, that is the particle tail of the bullet.
+    /// </summary>
+    public GameObject bulletSmoke;
+
+    /// <summary>
+    /// Global reference to the circle collider of destruction.
+    /// </summary>
+    public CircleCollider2D destructionCircle;
+
+    /// <summary>
+    /// Global reference to the ground.
+    /// </summary>
+    public static GroundController groundController;
+
+    /// <summary>
+    /// Start method.
+    /// </summary>
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+        //rb.velocity = new Vector2(5f, 10f);
+
+    /// <summary>
+    /// Update method.
+    /// </summary>
+    void Update()
+    {
+        if (updateAngle)
+        {
+            // Direction angle given by velocity.
+            Vector2 dir = new Vector2(rb.velocity.x, rb.velocity.y);
+
+            dir.Normalize();
+            float angle = Mathf.Asin(dir.y) * Mathf.Rad2Deg;
+            if (dir.x < 0f)
+            {
+                angle = 180 - angle;
+            }
+
+            //Debug.Log("angle = " + angle);
+
+            // Update of the Euler angles of the Sprite contained into the Bullet object.
+            bulletSpriteTransform.localEulerAngles = new Vector3(0f, 0f, angle + 45f);
+        }
+    }
+
+    /// <summary>
+    /// Catches every collision 2D.
+    /// </summary>
+    /// <param name="coll">Collision Event arguments.</param>
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        // When the bullet collides with another object, the bullet will explode,
+        // but only if the collided object has some specific attributes.
+        if (coll.collider.tag == "Ground")
+        {
+            updateAngle = false;
+            bulletSmoke.SetActive(false);
+            groundController.DestroyGround(destructionCircle);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GroundController.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+
+public class GroundController : MonoBehaviour
+{
+    // Reference variable for the SpriteRenderer component.
+    private SpriteRenderer spriteRenderer;
+
+    // Entire SpriteRenderer dimensions.
+    private float groundTotalWidth, groundTotalHeight;
+
+    // Ground image dimensions in pixel.
+    private int groundImgPixelWidth, groundImgPixelHeight;
+
+    // Utilities: Transparent color.
+    private Color colorTransparent;
+    
+    /// <summary>
+    /// Start method.
+    /// </summary>
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+        Texture2D textureOriginal = (Texture2D)Resources.Load("ground2");
+        // Resources.Load("nome_do_arquivo") carrega um arquivo localizado
+        // em Assets/Resources
+        Texture2D tex_clone = (Texture2D)Instantiate(textureOriginal);
+        // Criamos uma Texture2D clone de tex para nao alterarmos a imagem original 
+        spriteRenderer.sprite = Sprite.Create(tex_clone,
+                                  new Rect(0f, 0f, tex_clone.width, tex_clone.height),
+                                  new Vector2(0.5f, 0.5f), 100f);
+        colorTransparent = new Color(0f, 0f, 0f, 0f);
+        InitSpriteDimensions();
+        BulletController.groundController = this;
+
+        ResetPolygonCollider2D();
+    }
+
+    /// <summary>
+    /// Initializes global ground dimensions.
+    /// </summary>
+    private void InitSpriteDimensions()
+    {
+        groundTotalWidth = spriteRenderer.bounds.size.x;
+        groundTotalHeight = spriteRenderer.bounds.size.y;
+        groundImgPixelWidth = spriteRenderer.sprite.texture.width;
+        groundImgPixelHeight = spriteRenderer.sprite.texture.height;
+    }
+
+    /// <summary>
+    /// Update method.
+    /// </summary>
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Destroys part of the ground based on the CircleCollider2D dimension.
+    /// </summary>
+    /// <param name="destroyerCircleCollider">Collider that defines the area to destroy.</param>
+    public void DestroyGround(CircleCollider2D destroyerCircleCollider)
+    {
+        // Center coordinates of the collider.
+        V2int colliderCenter = World2Pixel(destroyerCircleCollider.bounds.center.x, destroyerCircleCollider.bounds.center.y);
+
+        // Collider ray.
+        int colliderRay = Mathf.RoundToInt(destroyerCircleCollider.bounds.size.x * groundImgPixelWidth / groundTotalWidth);
+        
+        int x, y, px, nx, py, ny, verticalCathetus;
+
+        // Moving X from 0 to the collider ray length...
+        for (x = 0; x <= colliderRay; x++)
+        {
+            // Every X defines a triangle.
+            // --> 0 to X is the horizontal cathetus.
+            // --> colliderRay is the hypotenuse.
+            // We need to calculate the vertical cathetus.
+            verticalCathetus = (int)Mathf.RoundToInt(Mathf.Sqrt(colliderRay * colliderRay - x * x));
+
+            // Moving Y from 0 to the vertical cathetus length...
+            for (y = 0; y <= verticalCathetus; y++)
+            {
+                // I am considering only one quarter, but with that I'm going to work in all the four ones.
+                // I need positive and negative coordinates of X and Y.
+                px = colliderCenter.x + x;
+                nx = colliderCenter.x - x;
+                py = colliderCenter.y + y;
+                ny = colliderCenter.y - y;
+
+                // For every singlecoordinate found by combining positive and negative X's and Y's,
+                // I am going to override the pixel positioned in that coordinate with
+                // the transparent color.
+                spriteRenderer.sprite.texture.SetPixel(px, py, colorTransparent);
+                spriteRenderer.sprite.texture.SetPixel(nx, py, colorTransparent);
+                spriteRenderer.sprite.texture.SetPixel(px, ny, colorTransparent);
+                spriteRenderer.sprite.texture.SetPixel(nx, ny, colorTransparent);
+            }
+        }
+        // Now I'll apply those updates on the texture. That's why I operate on a clone
+        // and not on the original texture.
+        spriteRenderer.sprite.texture.Apply();
+
+        // Now it's time to refresh the collider, that will ignore transparent pixels and fit
+        // the new texture.
+        ResetPolygonCollider2D();
+    }
+
+    /// <summary>
+    /// Gets the pixel coordinates from the position in the plane.
+    /// </summary>
+    /// <param name="x">X coordinate.</param>
+    /// <param name="y">Y coordinate.</param>
+    /// <returns></returns>
+    private V2int World2Pixel(float x, float y)
+    {
+        V2int v = new V2int();
+
+        float dx = x - transform.position.x;
+        v.x = Mathf.RoundToInt(0.5f * groundImgPixelWidth + dx * groundImgPixelWidth / groundTotalWidth);
+
+        float dy = y - transform.position.y;
+        v.y = Mathf.RoundToInt(0.5f * groundImgPixelHeight + dy * groundImgPixelHeight / groundTotalHeight);
+
+        return v;
+    }
+
+    /// <summary>
+    /// Resets the collider in order to fit the texture.
+    /// </summary>
+    private void ResetPolygonCollider2D() {
+        // Simple and funny: destroy and recreate.
+        Destroy(GetComponent<PolygonCollider2D>());
+        gameObject.AddComponent<PolygonCollider2D>();
+    }
+}

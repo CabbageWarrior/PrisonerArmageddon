1bd1d029a029b3f3656b5e09a72104e6561dd7f7
Fabio Brigidi <fabio.brigidi@gmal.com>
2017-07-03 22:01:12 +0200

    started implementing map generating script

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
 
-	/*
+
 	//public GameObject ground;
 	public int mapWidth, mapHeight;
 	public float groundPercBot, waterPercLeft, waterPercRight, featuresScale;
@@ -19,7 +19,7 @@ public class MapGenerator : MonoBehaviour {
 		//mapRenderer = ground.GetComponent<SpriteRenderer> ();
 		mapProfile = new Texture2D (mapWidth, mapHeight);
 		pixels = new float[mapProfile.width, mapProfile.height];
-		
+		CalcNoise ();
 	}
 	
 	// Update is called once per frame
@@ -29,47 +29,91 @@ public class MapGenerator : MonoBehaviour {
 
 	void CalcNoise()
 	{
-		float y = 0f;
+		float x = 0f, y = 0f;
 		while(x < mapProfile.width)
 		{
-			float xCoord = x / mapWidth * featuresScale;
-			float yCoord = y / mapWidth * featuresScale;
+			float xCoord = Mathf.Round(x / mapWidth * featuresScale);
+			float yCoord = Mathf.Round(y / mapWidth * featuresScale);
 			float sample = Mathf.PerlinNoise (xCoord, yCoord);
-			pixels [x][y] = sample;
+			pixels [(int)x, (int)y] = sample;
 			x++;
 		}
 
 		y++;
 	}
-	*/
-}
 
-// Experiments...
-/*
-public struct Coord : IStructuralEquatable<Coord>
-{
-	public Coord(int n, int m) : this()
+}
+	
+public class ClusterFinder{
+
+	private int n, m;
+	private float maskThreshold;
+	private float[,] matrix;
+	private int[,] mask;
+	private Cluster[] allClusters;
+
+	public ClusterFinder(float[,] matrix, int n, int m, float maskThreshold)
 	{
-		N = n;
-		M = m;
+		this.m = m;
+		this.n = n;
+		this.maskThreshold = maskThreshold;
+		this.matrix = matrix;
+		mask = new int[matrix.GetLength(0), matrix.GetLength(1)];
+		GetMask ();
 	}
 
-	public int N{ get; private set;}
-	public int M{ get; private set;}
-}
+	public void GetMask()
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++) 
+			{
+				if (matrix[i,j] > maskThreshold)
+					mask[i,j] = 1;
+				else
+					mask[i,j] = 0;
+			}
+		}
+	}
 
+	public void GetClusters()
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++) 
+			{
+				if (mask [i, j] = 1) 
+				{
+					thisCluster = new Cluster();
+
+				}
+			}
+		} 
+	}
+
+	public void FillCluster()
+	{
+		// Adds one point to the cluster and then set it to 0
+		// looks for non-zero neighbours
+		// Repeats for all neighbours until no neighbours are found
+	}
+
+}
 
 public class Cluster
 {
-	public Cluster(IEnumerable<Coord> coords)
-	{
-		if (coords == null)
-			throw new ArgumentNullException ("coords");
-
-		coords = coords;
-	}
-
-	public IEnumerable<Coord> Coords { get; private set;}
+	private Coordinates[] allCoordinates;
+	private bool isInThrehold;
 
 }
-*/
+
+public class Coordinates
+{
+	private int x, y;
+
+	public Coordinates(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+}

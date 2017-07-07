de8fb4391158478bb519d5aeb7f0aeda85c33e28
Fabio Brigidi <fabio.brigidi@gmail.com>
2017-07-07 10:38:49 +0200

    Updated charachters movement

--- Assets/Scripts/PrisonerMovement.cs
+++ Assets/Scripts/PrisonerMovement.cs
@@ -5,18 +5,19 @@ using UnityEngine.UI;
 
 public class PrisonerMovement : MonoBehaviour {
 
-	public float speed = 12f, maxVelocity = 3f, jumpForce = 4f, maxClimbAngle=60.0f;
+	public float speed = 12f, maxVelocity = 3f, jumpForce = 4f;
 	public LayerMask whatIsGround;
     //public Text text1, text2;
 
     //	private int whatIsGround = 1;
     private Rigidbody2D myBody;
 	[SerializeField]
-	private bool isJumping = false;
+	private bool isOnGround = false;
 	private Animator anim;
 	private float planeParallelX, planeParallelY, prisonerWidth, prisonerHeight;
+    private enum raysHitCondition {TwoHit, FrontHit, BackHit, NoHit};
 
-	void Awake()
+    void Awake()
 	{
 		myBody = GetComponent<Rigidbody2D> ();
         prisonerWidth = transform.localScale.x;
@@ -36,18 +37,18 @@ public class PrisonerMovement : MonoBehaviour {
 
 	void PrisonerMoveKeyboard()
 	{
-        float velocityX = 0f;
-        float velocityY = 0f;
+        float velocityX = 0f, velocityY = 0f;
+        float rayLength = 1.5f, rayShift = prisonerWidth / 2.5f;
         float slopeAngle;
 
         float distanceBack, distanceFront, distanceDifference;
-        // float vel = Mathf.Sqrt (Mathf.Pow(myBody.velocity.x, 2.0f) + Mathf.Pow(myBody.velocity.y, 2.0f));
         float vel = myBody.velocity.x;
         Vector2 backRayTransform, frontRayTransform;
 
-        float horizontalInput = Input.GetAxisRaw ("Horizontal");
-
-        if (!isJumping)
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        int raycastsCollisionsState;
+        
+        if (isOnGround)
         {   
             if (horizontalInput > 0)
             {
@@ -57,8 +58,8 @@ public class PrisonerMovement : MonoBehaviour {
                     ChangeDirection();
                 }
                 anim.SetBool("isWalking", true);
-                backRayTransform = new Vector2(transform.position.x - prisonerWidth/2.0f, transform.position.y);
-                frontRayTransform = new Vector2(transform.position.x + prisonerWidth / 2.0f, transform.position.y);
+                backRayTransform = new Vector2(transform.position.x - rayShift, transform.position.y);
+                frontRayTransform = new Vector2(transform.position.x + rayShift, transform.position.y);
             }
             else if (horizontalInput < 0)
             {
@@ -68,8 +69,8 @@ public class PrisonerMovement : MonoBehaviour {
                     ChangeDirection();
                 }
                 anim.SetBool("isWalking", true);
-                backRayTransform = new Vector2(transform.position.x + prisonerWidth / 2.0f, transform.position.y);
-                frontRayTransform = new Vector2(transform.position.x - prisonerWidth / 2.0f, transform.position.y);
+                backRayTransform = new Vector2(transform.position.x + rayShift, transform.position.y);
+                frontRayTransform = new Vector2(transform.position.x - rayShift, transform.position.y);
             }
             else
             {
@@ -79,78 +80,98 @@ public class PrisonerMovement : MonoBehaviour {
                     myBody.velocity = new Vector2(0, 0);
                 }
                 anim.SetBool("isWalking", false);
-                if (!isJumping)
+                if (isOnGround)
                 {
                     myBody.velocity = new Vector2(0, 0);
                 }
-                backRayTransform = new Vector2(transform.position.x - prisonerWidth / 2.0f, transform.position.y);
-                frontRayTransform = new Vector2(transform.position.x + prisonerWidth / 2.0f, transform.position.y);
+                //backRayTransform = new Vector2(transform.position.x - rayShift, transform.position.y);
+                //frontRayTransform = new Vector2(transform.position.x + rayShift, transform.position.y);
             }
 
             if (vel != 0)
             {
-                Debug.DrawRay(new Vector3(transform.position.x, transform.position.y, 0), new Vector3(2f * Mathf.Sign(horizontalInput), 0, 0));
-                Debug.DrawRay(new Vector3(backRayTransform.x, backRayTransform.y, 0), Vector3.down * 3f, Color.green);
-                Debug.DrawRay(new Vector3(frontRayTransform.x, frontRayTransform.y, 0), Vector3.down * 3f);
-
-                RaycastHit2D hitFront = Physics2D.Raycast(transform.position, new Vector2((int)Mathf.Sign(horizontalInput), 0), 2f, whatIsGround);
-                RaycastHit2D hitDownBack = Physics2D.Raycast(backRayTransform, Vector2.down, 3f, whatIsGround);
-                RaycastHit2D hitDownFront = Physics2D.Raycast(frontRayTransform, Vector2.down, 3f, whatIsGround);
+                Debug.DrawRay(new Vector3(backRayTransform.x, backRayTransform.y, 0), Vector3.down * rayLength, Color.green);
+                Debug.DrawRay(new Vector3(frontRayTransform.x, frontRayTransform.y, 0), Vector3.down * rayLength);
+           
+                RaycastHit2D hitDownBack = Physics2D.Raycast(backRayTransform, Vector2.down, rayLength, whatIsGround);
+                RaycastHit2D hitDownFront = Physics2D.Raycast(frontRayTransform, Vector2.down, rayLength, whatIsGround);
                 distanceBack = hitDownBack.distance;
-				//text1.text = distanceBack.ToString();
                 distanceFront = hitDownFront.distance;
-				//text2.text = distanceFront.ToString();
                 distanceDifference = distanceBack - distanceFront;
-                slopeAngle = Mathf.Rad2Deg * Mathf.Atan(distanceDifference/prisonerWidth);
 
-                // Checks the inclination of the terrain
-                if (hitFront.collider != null && (hitDownBack.collider != null || hitDownFront.collider != null))
-                {   
+                // Checks the inclination of the terrain and assign the right velocity direction
 
-                    if (slopeAngle <= maxClimbAngle)
-                    {
-                        if ((horizontalInput < 0 && distanceBack < distanceFront) || (horizontalInput > 0 && distanceBack > distanceFront))
+                if (hitDownBack.collider != null && hitDownFront.collider != null)
+                {
+                    raycastsCollisionsState = (int)raysHitCondition.TwoHit;
+                }
+                else if (hitDownBack.collider != null)
+                {
+                    raycastsCollisionsState = (int)raysHitCondition.BackHit;
+                }
+                else if (hitDownFront.collider != null)
+                {
+                    raycastsCollisionsState = (int)raysHitCondition.FrontHit;
+                }
+                else
+                {
+                    raycastsCollisionsState = (int)raysHitCondition.NoHit;
+                }
+
+
+                switch (raycastsCollisionsState)
+                {
+                    case (int)raysHitCondition.TwoHit:
+
+                        slopeAngle = Mathf.Rad2Deg * Mathf.Atan(distanceDifference / prisonerWidth);
+
+                        if (horizontalInput < 0)
                         {
                             velocityX = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * vel;
-                            velocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * vel;     
+                            velocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * -vel;
                         }
                         else
                         {
                             velocityX = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * vel;
-                            velocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * -vel;                
+                            velocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * vel;
                         }
-                    }
-                    else
-                    {
-						velocityX = vel;
-						velocityY = 0f;
-                    }
+
+                        if (Mathf.Min(distanceBack, distanceFront) > prisonerHeight / 2.0f)
+                        {
+                            velocityY -= 1;
+                        }
+
+                        break;
+
+                    case (int)raysHitCondition.FrontHit:
+
+                        
+                        velocityX = vel;
+                        velocityY = 0f;
+                       
+                        if (Mathf.Min(distanceBack, distanceFront) > prisonerHeight / 2.0f)
+                        {
+                            velocityY -= 1f;
+                        }
+
+                        break;
+
+                    case (int)raysHitCondition.BackHit:
+
+                        velocityX = vel;
+                        velocityY = -5f;
+
+                        break;
+
+                    default:
+
+                        velocityX = 0f;
+                        velocityY = 0f;
+                        isOnGround = false;
+                        break;
                 }
 
-                else if (hitFront.collider == null && (hitDownBack.collider != null || hitDownFront.collider != null))
-                {
-                    
-                    if (horizontalInput < 0 && distanceBack < distanceFront)
-                    {
-                        velocityX = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * vel;
-                        velocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * -vel;
-                    }
-                    else
-                    {
-                        velocityX = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * vel;
-                        velocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * vel;
-                    }
-                }
-                else
-                {	
-					velocityX = vel;
-					velocityY = 0f;
-                }
-
-				if (Mathf.Min(distanceBack, distanceFront) > prisonerHeight/2.0f)
-				{
-					velocityY -= 1;
-				}
+				
 				myBody.velocity = new Vector2(velocityX, velocityY);
 
             }
@@ -160,14 +181,10 @@ public class PrisonerMovement : MonoBehaviour {
                 myBody.velocity = new Vector2(0, 0);
             }
 
-            if (Input.GetButton("Jump") && !isJumping)
+            if (Input.GetButton("Jump") && isOnGround)
             {
-				Jump ();
+                Jump();
             }
-
-            //text1.text = velocityX.ToString();
-            //text2.text = velocityY.ToString();
-
         }
 
 	}
@@ -184,7 +201,7 @@ public class PrisonerMovement : MonoBehaviour {
 	{
 		if (other.gameObject.tag == "Ground")
 		{
-			isJumping = false;
+			isOnGround = true;
 			anim.SetBool ("isJumping", false);
 			myBody.velocity = new Vector2 (0, 0);
 		}
@@ -193,7 +210,7 @@ public class PrisonerMovement : MonoBehaviour {
 
 	void Jump()
 	{
-		isJumping = true;
+		isOnGround = false;
 		myBody.velocity = new Vector2(0, 0);
 		anim.SetBool("isJumping", true);
 		if (transform.localScale.x > 0f) 
